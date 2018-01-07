1a3e274ebeb81686eb619b6140ab3f7f4237c215
eygle <cavalec.johan@gmail.com>
2018-01-07 21:21:51 +0100

    Add server routes proxy

---
+++ proxy.conf.js
@@ -0,0 +1,18 @@
+const PROXY_CONFIG = [
+  {
+    context: [
+      "/api",
+      "/register",
+      "/login",
+      "/logout",
+      "/forgot-password",
+      "/change-password",
+      "/unlock-user",
+      "/unsubscribe"
+    ],
+    target: "http://localhost:4242",
+    secure: false
+  }
+];
+
+module.exports = PROXY_CONFIG;
